5b8a4a30b282f360dd054fd0f65f48e42f98a32f
Melvin-579 <90847738+Melvin-579@users.noreply.github.com>
2023-05-17 23:50:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/material3/MainActivity.kt
@@ -0,0 +1,106 @@
+
+
+package com.example.material3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.isContainer
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import com.example.material3.ui.theme.Material3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Material3Theme {
+               MainScreen()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    var text by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+
+    Scaffold {
+        Box(Modifier.fillMaxSize()) {
+            // Talkback focus order sorts based on x and y position before considering z-index. The
+            // extra Box with semantics and fillMaxWidth is a workaround to get the search bar to focus
+            // before the content.
+            Box(Modifier.semantics { isContainer = true }.zIndex(1f).fillMaxWidth()) {
+                SearchBar(
+                    modifier = Modifier.align(Alignment.TopCenter),
+                    query = text,
+                    onQueryChange = { text = it },
+                    onSearch = { active = false },
+                    active = active,
+                    onActiveChange = {
+                        active = it
+                    },
+                    placeholder = { Text("Hinted search text") },
+                    leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+                    trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) },
+                ) {
+                    LazyColumn(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentPadding = PaddingValues(16.dp),
+                        verticalArrangement = Arrangement.spacedBy(4.dp)
+                    ) {
+                        items(4) { idx ->
+                            val resultText = "Suggestion $idx"
+                            ListItem(
+                                headlineContent = { Text(resultText) },
+                                supportingContent = { Text("Additional info") },
+                                leadingContent = { Icon(Icons.Filled.Star, contentDescription = null) },
+                                modifier = Modifier.clickable {
+                                    text = resultText
+                                    active = false
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+
+            LazyColumn(
+                contentPadding = PaddingValues(start = 16.dp, top = 72.dp, end = 16.dp, bottom = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                val list = List(100) { "Text $it" }
+                items(count = list.size) {
+                    Text(list[it], Modifier.fillMaxWidth().padding(horizontal = 16.dp))
+                }
+            }
+        }
+}
+
+
+
+@Preview(showBackground = false)
+@Composable
+fun DefaultPreview() {
+    Material3Theme {
+        MainScreen()
+    }
+}

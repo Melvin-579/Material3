763846420521a24f09850749b010a363094db04b
Melvin-579 <90847738+Melvin-579@users.noreply.github.com>
2023-05-18 13:16:33 -0400

    we failed

--- app/src/main/java/com/example/material3/MainActivity.kt
+++ app/src/main/java/com/example/material3/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
@@ -19,9 +20,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.isContainer
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.example.material3.ui.theme.Material3Theme
@@ -48,7 +51,7 @@ fun MainScreen() {
             // Talkback focus order sorts based on x and y position before considering z-index. The
             // extra Box with semantics and fillMaxWidth is a workaround to get the search bar to focus
             // before the content.
-            Box(Modifier.semantics { isContainer = true }.zIndex(1f).fillMaxWidth()) {
+            //Box(Modifier.semantics { isContainer = true }.zIndex(1f).fillMaxWidth()) {
                 SearchBar(
                     modifier = Modifier.align(Alignment.TopCenter),
                     query = text,
@@ -62,7 +65,7 @@ fun MainScreen() {
                     leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
                     trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) },
                 ) {
-                    LazyColumn(
+                    /*LazyColumn(
                         modifier = Modifier.fillMaxWidth(),
                         contentPadding = PaddingValues(16.dp),
                         verticalArrangement = Arrangement.spacedBy(4.dp)
@@ -72,35 +75,73 @@ fun MainScreen() {
                             ListItem(
                                 headlineContent = { Text(resultText) },
                                 supportingContent = { Text("Additional info") },
-                                leadingContent = { Icon(Icons.Filled.Star, contentDescription = null) },
+                                leadingContent = {
+                                    Icon(
+                                        Icons.Filled.Star,
+                                        contentDescription = null
+                                    )
+                                },
                                 modifier = Modifier.clickable {
                                     text = resultText
                                     active = false
                                 }
                             )
                         }
-                    }
+                    }*/
                 }
-            }
+            //}
 
-            LazyColumn(
-                contentPadding = PaddingValues(start = 16.dp, top = 72.dp, end = 16.dp, bottom = 16.dp),
+        /*    LazyColumn(
+                contentPadding = PaddingValues(
+                    start = 16.dp,
+                    top = 72.dp,
+                    end = 16.dp,
+                    bottom = 16.dp
+                ),
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
                 val list = List(100) { "Text $it" }
                 items(count = list.size) {
                     Text(list[it], Modifier.fillMaxWidth().padding(horizontal = 16.dp))
                 }
-            }
+            }*/
         }
+    }
+
 }
 
+@Composable
+fun NavigationBar(
+    modifier: Modifier = Modifier,
+    containerColor: Color = NavigationBarDefaults.containerColor,
+    contentColor: Color = MaterialTheme.colorScheme.contentColorFor(containerColor),
+    tonalElevation: Dp = NavigationBarDefaults.Elevation,
+    windowInsets: WindowInsets = NavigationBarDefaults.windowInsets,
+    content: @Composable RowScope.() -> Unit
+): Unit {
+}
 
+@Composable
+fun Nav2() {
+    var selectedItem by remember { mutableStateOf(0) }
+    val items = listOf("Songs", "Artists", "Playlists")
 
-@Preview(showBackground = false)
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                label = { Text(item) },
+                selected = selectedItem == index,
+                onClick = { selectedItem = index }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Material3Theme {
-        MainScreen()
+    Nav2()
     }
 }
